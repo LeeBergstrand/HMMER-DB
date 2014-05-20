fb4cb3b38c5c7e3284adcef2494a52262fbae322
Lee Bergstrand <leemacboy@gmail.com>
2014-05-20 15:33:46 -0700

    Perl Magic.

--- ReferenceScripts/Expanded-HMMscanParser.pl
+++ ReferenceScripts/Expanded-HMMscanParser.pl
@@ -52,32 +52,39 @@ while(<>)
 	chomp;
 	@a=split;
 	next if $a[-1]==$a[-2];
-	push(@{$b{$a[0]}},$_);
+	push(@{$b{$a[0]}},$_); # Removes lines with same start and end
 }
-foreach(sort keys %b){@a=@{$b{$_}};
 
-for($i=0;$i<$#a;$i++)
+foreach(sort keys %b) 
 {
-	@b=split(/\t/,$a[$i]);
-	@c=split(/\t/,$a[$i+1]);
-	$len1=$b[-1]-$b[-2];
-	$len2=$c[-1]-$c[-2];
-	$len3=$b[-1]-$c[-2];
-	
-	if($len3>0 and ($len3/$len1>0.5 or $len3/$len2>0.5))
-	{
-		if($b[2]<$c[2])
+        @a=@{$b{$_}};
+
+        for($i = 0; $i < $#a; $i++) 
 		{
-			splice(@a,$i+1,1);
-		}
-		else
+                @b = split(/\t/, $a[$i]);   # Alignment 1 = top alignment
+                @c = split(/\t/, $a[$i+1]); # Alignment 2 = bottom alignment
+                $len1 = $b[-1] - $b[-2]; # Length one = aligment 1 end - aligment 1 start
+                $len2 = $c[-1] - $c[-2]; # Length two = aligment 2 end - aligment 2 start
+                $len3 = $b[-1] - $c[-2]; # Length one = aligment 1 end - aligment 2 start
+
+                if($len3 > 0 and ($len3 / $len1 > 0.5 or $len3 / $len2 > 0.5)) # if alignments are overlaped and the overlap is greater than 50% the length of an alignment. 
+                {
+                        if($b[2] < $c[2]) # Checks E value. Removes the alignment with the lowest Evalue.
+                        {
+                                splice(@a, $i + 1, 1);
+                        }
+                        else
+                        {
+                                splice(@a, $i, 1);
+                        }
+                        $i = $i - 1;
+                }
+        }
+        foreach(@a) 
 		{
-			splice(@a,$i,1);
-		}
-		$i=$i-1;
-	}
+                print $_ . "\n";
+        }
 }
-foreach(@a){print $_."\n";}} 
 
 # | uniq | perl -e 
 open(IN,"all.hmm.ps.len");
@@ -85,14 +92,16 @@ while(<IN>)
 {
 	chomp;
 	@a=split;
-	$b{$a[0]}=$a[1];}
-	while(<>)
-	{
-		chomp;
-		@a=split;
-		$r=($a[4]-$a[3])/$b{$a[1]};
-		print $_."\t".$r."\n";
-	}
+	$b{$a[0]}=$a[1]; # creates hash of hmmName : hmmLength
+}
+
+while(<>)
+{
+	chomp;
+	@a=split;
+	$r=($a[4]-$a[3])/$b{$a[1]}; # $a[4] = hmm end $a[3] = hmm start ; $b{$a[1]} = result of the hash of the name of the hmm.
+	print $_."\t".$r."\n";
+}
 	
 # | perl -e 
 while(<>)
