005901e30b58defb274227bfbd7313a5a0525dd4
Lee Bergstrand <leemacboy@gmail.com>
2014-05-26 10:36:37 -0700

    Updating to for HMMExtract.py to run off of output from Supplementary scripts.

--- HMMExtract.py
+++ HMMExtract.py
@@ -17,6 +17,7 @@ import sys
 import subprocess
 import sqlite3
 import cStringIO
+import csv
 import re
 from os import path
 from Bio import SeqIO 
@@ -49,24 +50,20 @@ def runHMMSearch(FASTA, HMMERDBFile):
 	else:
 		return stdout
 #------------------------------------------------------------------------------------------------------------
-# 3: When passed a sequence record object returns an array of fasta strings for each annotation.
-def getProtienAnnotationFasta(seqRecord):
-	fasta = {}
-	features = seqRecord.features # Each sequence has a list (called features) that stores seqFeature objects.
-	for feature in features: # For each feature on the sequence
-		if feature.type == "CDS": # CDS means coding sequence (These are the only feature we're interested in)
-			featQualifers = feature.qualifiers # Each feature contains a dictionary called quailifiers which contains           
-			                                   # data about the sequence feature (for example the translation)
-			
-			# Gets the required qualifers. Uses featQualifers.get to return the quatifer or a default value if the quatifer			# is not found. Calls strip to remove unwanted brackets and ' from quantifer before storing it as a string.
-			protein_id = str(featQualifers.get('protein_id','no_protein_id')).strip('\'[]')
-			if protein_id == 'no_protein_id':
-				continue # Skips the iteration if protien has no id.
-			gene    = str(featQualifers.get('gene','no_gene_name')).strip('\'[]')
-			product = str(featQualifers.get('product','no_product_name')).strip('\'[]')
-			translated_protein = str(featQualifers.get('translation','no_translation')).strip('\'[]')
-			fasta[protein_id] = (">" + protein_id + " " + gene + "-" + product + "\n" + translated_protein + "\n")
-	return fasta
+# 3: Creates a python dictionary (hash table) that contains the the fasta for each protien in the proteome.
+def createProteomeHash(ProteomeFile):
+	ProteomeHash = dict() 
+	try:
+		handle = open(ProteomeFile, "rU")
+		proteome = SeqIO.parse(handle, "fasta")
+		for record in proteome:
+			ProteomeHash.update({ record.id : record.format("fasta") })
+		handle.close()
+	except IOError:
+		print "Failed to open " + ProteomeFile
+		exit(1)
+		
+	return ProteomeHash
 #------------------------------------------------------------------------------------------------------------
 # 4: Parses HMM searches text output and generates a two dementional array of the domain alignments results.
 def parseHmmsearchResults(HMMResults, HMMName, HMMLength):
@@ -94,7 +91,7 @@ def parseHmmsearchResults(HMMResults, HMMName, HMMLength):
 #------------------------------------------------------------------------------------------------------------
 # 6: Fitres HMM Hits.
 def fitreHMMHitTable(HMMHitTable):
-	#HMMHitTable[:] = [row for row in HMMHitTable if row[3] < float("1e-30")] # Filtres by E-value.
+	HMMHitTable[:] = [row for row in HMMHitTable if row[3] < float("1e-30")] # Filtres by E-value.
 	
 	FiltredHMMHitTable = list(HMMHitTable)
 	for i in range(0,(len(HMMHitTable)-1)):
@@ -109,15 +106,15 @@ def fitreHMMHitTable(HMMHitTable):
 				print "Overlap"
 				# If the overlap is greater than 50% of either alignment.
 				if((((float(Overlap)/float(AlignmentOneLength)) > 0.5) or ((float(Overlap)/float(AlignmentOneLength)) > 0.5))): 
-				print "Is greater than 50"
-						HMMHitTable.remove(RowOne) # The alignment with the lower evalue will allways be in the second row since 
-												    # HMMSearch always formats the output with multiple domains highest to lowest E-value.
+					print "Is greater than 50"
+					HMMHitTable.remove(RowOne) # The alignment with the lower evalue will allways be in the second row since 
+											    # HMMSearch always formats the output with multiple domains highest to lowest E-value.
 				else:
 					print "Is greater than 50"
 			else:
 				print "No Overlap"
 	
-	#HMMHitTable[:] = [row for row in HMMHitTable if row[-1] > 0.3] # Filtres by Query Coverage.
+	HMMHitTable[:] = [row for row in HMMHitTable if row[-1] > 0.3] # Filtres by Query Coverage.
 	return	HMMHitTable		
 #------------------------------------------------------------------------------------------------------------
 # 6: Creates list of hits protien FASTAs.
@@ -130,20 +127,23 @@ def getHitProteins(HMMHitTable, AnnotationFASTADict):
 # Main program code:
 	
 # House keeping...
-argsCheck(4) # Checks if the number of arguments are correct.
+argsCheck(5) # Checks if the number of arguments are correct.
 
 # Stores file one for input checking.
 print ">> Starting up..."
 OrganismFile = sys.argv[1]
-HMMFile      = sys.argv[2]
-sqlFile      = sys.argv[3]
+PhyloCSVFile = sys.argv[2]
+HMMFile      = sys.argv[3]
+sqlFile      = sys.argv[4]
 
 HMMName = path.split(HMMFile)[1].rstrip(".hmm")
 
 # File extension checks
 print ">> Performing file extention checks..."
-if not OrganismFile.endswith(".gbk"):
-	print "[Warning] " + OrganismFile + " may not be a genbank file!"
+if not OrganismFile.endswith(".faa"):
+	print "[Warning] " + OrganismFile + " may not be a fasta file!"
+if not PhyloCSVFile.endswith(".csv"):
+	print "[Warning] " + PhyloCSVFile + " may not be a csv file!"
 if not HMMFile.endswith(".hmm"):
 	print "[Warning] " + HMMFile + " may not be a HMM file!"
 if not sqlFile.endswith(".sqlite"):
@@ -151,12 +151,24 @@ if not sqlFile.endswith(".sqlite"):
 
 # Read in genbank file as a sequence record object.
 try:
-	print ">> Opening Genbank File..."
+	print ">> Opening FASTA File..."
 	handle = open(OrganismFile, "rU")
-	record = SeqIO.read(handle, "genbank")
+	records = SeqIO.parse(handle, "fasta")
 	handle.close()
 except IOError:
 	print "Failed to open " + OrganismFile
+	sys.exit(1)
+	
+# Opens CSV file for reading.
+try:
+	print ">> Opening CSV File: " + PhyloCSVFile
+	readFile = open(PhyloCSVFile, "r")
+	reader  = csv.reader(readFile) # opens file with csv module which takes into account verying csv formats and parses correctly.
+	print type(reader)
+	print ">> Good CSV file."
+	readFile.close()
+except IOError:
+	print "Failed to open " + PhyloCSVFile
 	sys.exit(1)
 
 # Gets HMM Length.
@@ -174,33 +186,35 @@ except IOError:
 	sys.exit(1)
 	
 print ">> Extracting Protein Annotations..."
-AnnotationFASTADict = getProtienAnnotationFasta(record) # Creates a dictionary containing all protein annotations in the gbk file.
+AnnotationFASTADict = createProteomeHash(OrganismFile) # Creates a dictionary containing all protein annotations in the gbk file.
 print ">> Extracting Organism Info..."
 
-OrganismInfo = [record.annotations['source'], record.annotations['taxonomy'][0], record.annotations['taxonomy']]
-FASTAString = "".join(AnnotationFASTADict.values()) # Saves these annotations to a string.
+#OrganismInfo = [record.annotations['source'], record.annotations['taxonomy'][0], record.annotations['taxonomy']]
+
+
+#FASTAString = "".join(AnnotationFASTADict.values()) # Saves these annotations to a string.
 
 #HMMResults = runHMMSearch(FASTAString, HMMFile) # Runs hmmsearch.
 
 #HMMHitTable = parseHmmsearchResults(HMMResults, HMMName, HMMLength) # Parses hmmsearch results into a two dimensional array.
 
-HMMHitTable = [['YP_705149.1', 'Cyp125(GramPos)', 1.3, 42.0, 99, 132, 104, 137, 0.08088235294117647]]
-HMMHitTable.append(['YP_705149.1', 'Cyp125(GramPos)', 30.4, 5.9e-08, 214, 376, 230, 420, 0.39705882352941174])
+#HMMHitTable = [['YP_705149.1', 'Cyp125(GramPos)', 1.3, 42.0, 99, 132, 104, 137, 0.08088235294117647]]
+#HMMHitTable.append(['YP_705149.1', 'Cyp125(GramPos)', 30.4, 5.9e-08, 214, 376, 230, 420, 0.39705882352941174])
 
-HMMHitTable.append(['YP_703037.1', 'Cyp125(GramPos)', 2.7, 16.0, 100, 130, 107, 236, 0.07352941176470588])
-HMMHitTable.append(['YP_703037.1', 'Cyp125(GramPos)', 11.5, 0.035, 213, 275, 231, 293, 0.15196078431372548])
-HMMHitTable.append(['YP_703037.1', 'Cyp125(GramPos)', 18.1, 0.00034, 286, 376, 323, 422, 0.22058823529411764])
+#HMMHitTable.append(['YP_703037.1', 'Cyp125(GramPos)', 2.7, 16.0, 100, 130, 107, 236, 0.07352941176470588])
+#HMMHitTable.append(['YP_703037.1', 'Cyp125(GramPos)', 11.5, 0.035, 213, 275, 231, 293, 0.15196078431372548])
+#HMMHitTable.append(['YP_703037.1', 'Cyp125(GramPos)', 18.1, 0.00034, 286, 376, 323, 422, 0.22058823529411764])
 
-HMMHitTable.append(['YP_704615.1', 'Cyp125(GramPos)', 6.1, 1.4, 198, 273, 196, 278, 0.18382352941176472])
-HMMHitTable.append(['YP_704615.1', 'Cyp125(GramPos)', 26.7, 7.9e-07, 283, 377, 308, 411, 0.23039215686274508])
+#HMMHitTable.append(['YP_704615.1', 'Cyp125(GramPos)', 6.1, 1.4, 198, 273, 196, 278, 0.18382352941176472])
+#HMMHitTable.append(['YP_704615.1', 'Cyp125(GramPos)', 26.7, 7.9e-07, 283, 377, 308, 411, 0.23039215686274508])
 
-HMMHitTable.append(['YP_704571.1', 'Cyp125(GramPos)', -3.4, 1100.0, 104, 148, 136, 180, 0.10784313725490197])
-HMMHitTable.append(['YP_704571.1', 'Cyp125(GramPos)', 14.5, 0.0041, 213, 377, 270, 464, 0.4019607843137255])
+#HMMHitTable.append(['YP_704571.1', 'Cyp125(GramPos)', -3.4, 1100.0, 104, 148, 136, 180, 0.10784313725490197])
+#HMMHitTable.append(['YP_704571.1', 'Cyp125(GramPos)', 14.5, 0.0041, 213, 377, 270, 464, 0.4019607843137255])
 
 
 
-for i in HMMHitTable:
-	print i 
-HMMHitTable = fitreHMMHitTable(HMMHitTable)
+#for i in HMMHitTable:
+#	print i 
+#HMMHitTable = fitreHMMHitTable(HMMHitTable)
 
 # HitProtienFASTAs = getHitProteins(HMMHitTable, AnnotationFASTADict) # Gets hit protein FASTAs.
