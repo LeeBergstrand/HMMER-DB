b9fbb86f4c530135dab2067ca903dd18ac1426d4
Lee Bergstrand <leemacboy@gmail.com>
2014-05-30 11:11:48 -0700

    Added shell script to log multiple runs.

---
+++ SupplementaryScripts/runHMMExtract.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# A simple script for the batch running of HMMExtract.py.
+
+timestamp=$(date +"%T")
+
+for HMM;
+do
+	echo Running HMMExtract on $HMM
+	time find ./ -name "*.faa" | parallel --gnu --progress -k -j +0 python ../HMMExtract.py {} OrganismDB.csv $HMM ../TestDBs/TestDB.sqlite 2>&1 | tee -a "Log$timestamp.txt"
+	date +"%T" >> "Log$timestamp.txt"
+	echo 
+done
+echo All hmms searched.
+ 
+exit 0
+
