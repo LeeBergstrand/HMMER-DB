a9b0e78a6fd7f57c87000ebc16b1c2ef0fa397a2
Lee Bergstrand <leemacboy@gmail.com>
2014-06-27 09:57:20 -0700

    Updated .gitignore to account for R markdown. Update script for running HMMER-DB. Starting to convert visualization scripts from plain R to R markdown.

--- SupplementaryScripts/runHMMExtract.sh
+++ SupplementaryScripts/runHMMExtract.sh
@@ -6,7 +6,7 @@ timestamp=$(date +"%T")
 for HMM;
 do
 	echo Running HMMExtract on $HMM
-	time find ./ -name "*.faa" | parallel --gnu --progress -k -j +0 python ../HMMExtract.py {} OrganismDB.csv $HMM ../TestDBs/TestDB.sqlite 2>&1 | tee -a "Log$timestamp.txt"
+	time find ./ -name "*.faa" | parallel --gnu --progress -k -j +0 python ./SearchExtractLoad.py {} OrganismDB.csv $HMM ./TestDBs/TestDB.sqlite 2>&1 | tee -a "Log$timestamp.txt"
 	date +"%T" >> "Log$timestamp.txt"
 	echo 
 done
