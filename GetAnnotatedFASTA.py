aad542910eb1d1d7e737b3610ca6f9cb8705c02e
Lee Bergstrand <leemacboy@gmail.com>
2015-07-24 16:27:02 -0700

    Cleaning up and reformatting HMMER-DB

--- GetAnnotatedFASTA.py
+++ GetAnnotatedFASTA.py
@@ -1,111 +1,114 @@
 #!/usr/bin/env python 
 # Created by: Lee Bergstrand 
-# Descript: A program that extracts the protiens annotations from a genbank file and as well as some 
+# Descript: A program that extracts the proteins annotations from a Genbank file and as well as some
 #			information about the organism in the file. 
 #
 # Requirements: - This script requires the Biopython module: http://biopython.org/wiki/Download
 #
 # Usage: GenbankToFASTAandOrganismTable.py <organism.gbk> 
 # Example: GenbankToFASTAandOrganismTable.py ecoli.gbk
-#----------------------------------------------------------------------------------------
-#===========================================================================================================
-#Imports & Setup:
-	
+# ----------------------------------------------------------------------------------------
+# ===========================================================================================================
+# Imports & Setup:
+
 import sys
 from Bio import SeqIO
 from os import path
 
-#===========================================================================================================
+
+# ===========================================================================================================
 # Functions:
 
 # 1: Checks if in proper number of arguments are passed gives instructions on proper use.
 def argsCheck(numArgs):
 	if len(sys.argv) < numArgs or len(sys.argv) > numArgs:
-		print "Extracts annotation information and organism info from a genbank file."
-		print "By Lee Bergstrand\n"
-		print "Usage: " + sys.argv[0] + " <organism.gbk>"
-		print "Examples: " + sys.argv[0] + " ecoli.gbk"
-		sys.exit(1) # Aborts program. (exit(1) indicates that an error occurred)
+		print("Extracts annotation information and organism info from a genbank file.")
+		print("By Lee Bergstrand\n")
+		print("Usage: " + sys.argv[0] + " <organism.gbk>")
+		print("Examples: " + sys.argv[0] + " ecoli.gbk")
+		sys.exit(1)  # Aborts program. (exit(1) indicates that an error occurred)
 
-#------------------------------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------------------
 # 2: When passed a sequence record object returns an array of fasta strings for each annotation.
 def getProtienAnnotationFasta(seqRecord):
 	fasta = []
-	features = seqRecord.features # Each sequence has a list (called features) that stores seqFeature objects.
-	for feature in features: # For each feature on the sequence
-		if feature.type == "CDS": # CDS means coding sequence (These are the only feature we're interested in)
-			featQualifers = feature.qualifiers # Each feature contains a dictionary called quailifiers which contains           
-			                                   # data about the sequence feature (for example the translation)
-			
-			start  = int(feature.location.start) # Type-casting to int strips fuzzy < > characters.
-			end    = int(feature.location.end)
+	features = seqRecord.features  # Each sequence has a list (called features) that stores seqFeature objects.
+	for feature in features:  # For each feature on the sequence
+		if feature.type == "CDS":  # CDS means coding sequence (These are the only feature we're interested in)
+			featQualifers = feature.qualifiers  # Each feature contains a dictionary called qualifiers which contains
+			#  data about the sequence feature (for example the translation)
+
+			start = int(feature.location.start)  # Type-casting to int strips fuzzy < > characters.
+			end = int(feature.location.end)
 			strand = feature.location.strand
-			if strand == None:
+			if strand is None:
 				strand = "?"
 			elif int(strand) < 0:
 				strand = "-"
 			elif int(strand) > 0:
-				strand =  "+"
+				strand = "+"
 			else:
 				strand = "?"
-			
+
 			location = "[" + str(start) + ":" + str(end) + "](" + strand + ")"
-			
-			# Gets the required qualifers. Uses featQualifers.get to return the quatifer or a default value if the quatifer
-			# is not found. Calls strip to remove unwanted brackets and ' from quantifer before storing it as a string.
-			protein_id = str(featQualifers.get('protein_id','no_protein_id')).strip('\'[]')
+
+			# Gets the required qualifiers. Uses featQualifiers.get to return the qualifier or a default value if the qualifier
+			# is not found. Calls strip to remove unwanted brackets and ' from qualifier before storing it as a string.
+			protein_id = str(featQualifers.get('protein_id', 'no_protein_id')).strip('\'[]')
 			if protein_id == 'no_protein_id':
-				continue # Skips the iteration if protien has no id.
-			protein_locus = str(featQualifers.get('locus_tag','no_locus_tag')).strip('\'[]')
-			gene    = str(featQualifers.get('gene','no_gene_name')).strip('\'[]')
-			product = str(featQualifers.get('product','no_product_name')).strip('\'[]')
-			translated_protein = str(featQualifers.get('translation','no_translation')).strip('\'[]')
-			fasta.append(">" + protein_id + " " + gene + "-" + product + " (Locus: " + protein_locus + ")" + " (Location: " + location + ")" + "\n" + translated_protein + "\n")
+				continue  # Skips the iteration if protein has no id.
+			protein_locus = str(featQualifers.get('locus_tag', 'no_locus_tag')).strip('\'[]')
+			gene = str(featQualifers.get('gene', 'no_gene_name')).strip('\'[]')
+			product = str(featQualifers.get('product', 'no_product_name')).strip('\'[]')
+			translated_protein = str(featQualifers.get('translation', 'no_translation')).strip('\'[]')
+			fasta.append(
+				">" + protein_id + " " + gene + "-" + product + " (Locus: " + protein_locus + ")" + " (Location: " + location + ")" + "\n" + translated_protein + "\n")
 	FASTAString = "".join(fasta)
 	return FASTAString
-		
-#------------------------------------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------------------------------------
 # Main program code:
 
 # House keeping...
-argsCheck(2) # Checks if the number of arguments are correct.
+argsCheck(2)  # Checks if the number of arguments are correct.
 
 # Stores file one for input checking.
-print ">> Starting up..."
+print(">> Starting up...")
 OrganismFile = sys.argv[1]
 OrganismID = path.split(OrganismFile)[1].rstrip(".gbk")
 FastaFile = OrganismID + ".faa"
 
 # File extension checks
-print ">> Performing file extention checks..."
+print(">> Performing file extension checks...")
 if not OrganismFile.endswith(".gbk"):
-	print "[Warning] " + OrganismFile + " may not be a genbank file!"
+	print("[Warning] " + OrganismFile + " may not be a genbank file!")
 
 # Read in genbank file as a sequence record object.
 try:
-	print ">> Opening " + OrganismFile
+	print(">> Opening " + OrganismFile)
 	handle = open(OrganismFile, "rU")
 	try:
 		record = SeqIO.read(handle, "genbank")
 	except ValueError as error:
-		print "Error has occured while parsing " + OrganismFile + "!"
-		print error
-		print error.traceback
+		print("Error has occured while parsing " + OrganismFile + "!")
+		print(str(error))
 		sys.exit(1)
 	handle.close()
 except IOError:
-	print "Failed to open " + OrganismFile
+	print("Failed to open " + OrganismFile)
 	sys.exit(1)
-	
-print ">> Extracting Protein Annotations..."
-FASTA = getProtienAnnotationFasta(record) # Creates a string containing all protein annotations in the gbk file.
+
+print(">> Extracting Protein Annotations...")
+FASTA = getProtienAnnotationFasta(record)  # Creates a string containing all protein annotations in the gbk file.
 
 # Writes annotations to FASTA file.
 try:
-	print ">> Writing FASTA File..."
+	print(">> Writing FASTA File...")
 	FASTAWriter = open(FastaFile, "w")
 	FASTAWriter.write(FASTA)
 	FASTAWriter.close()
 except IOError:
-	print "Failed to open " + FastaFile
+	print("Failed to open " + FastaFile)
 	sys.exit(1)
--- SearchExtractLoad.py
+++ SearchExtractLoad.py
@@ -10,10 +10,10 @@
 #
 # Usage: HMMExtract.py <organism.faa> <organisms.csv> <hmm.hmm> <sqldb.sqlite>
 # Example: HMMExtract.py ecoli.faa bacteria.csv helicase.hmm helicasedb.sqlite
-#----------------------------------------------------------------------------------------
-#===========================================================================================================
-#Imports & Setup:
-	
+# ----------------------------------------------------------------------------------------
+# ===========================================================================================================
+# Imports & Setup:
+
 import sys
 import subprocess
 import sqlite3
@@ -21,256 +21,320 @@ import csv
 import re
 import hashlib
 from os import path
-from Bio import SeqIO 
+from Bio import SeqIO
 from multiprocessing import cpu_count
 
-processors = cpu_count() # Gets number of processor cores for HMMER.
+processors = cpu_count()  # Gets number of processor cores for HMMER.
 
 # Regex's
 LocusRegex = re.compile("\(Locus:\s\S*\)")
 LocationRegex = re.compile("\(Location:\s\[(\S*)\:(\S*)\]\((\S)\)\)")
-#===========================================================================================================
+
+
+# ===========================================================================================================
 # Functions:
 
 # 1: Checks if in proper number of arguments are passed gives instructions on proper use.
 def argsCheck(numArgs):
 	if len(sys.argv) < numArgs or len(sys.argv) > numArgs:
-		print "Hmmer hit cataloguer."
-		print "By Lee Bergstrand\n"
-		print "Usage: " + sys.argv[0] + "  <organism.faa> <organisms.csv> <hmm.hmm> <sqldb.sqlite>"
-		print "Examples: " + sys.argv[0] + "  <organism.faa> <organisms.csv> <hmm.hmm> <sqldb.sqlite>"
-		sys.exit(1) # Aborts program. (exit(1) indicates that an error occurred)
-#------------------------------------------------------------------------------------------------------------
+		print("Hmmer hit cataloguer.")
+		print("By Lee Bergstrand\n")
+		print("Usage: " + sys.argv[0] + "  <organism.faa> <organisms.csv> <hmm.hmm> <sqldb.sqlite>")
+		print("Examples: " + sys.argv[0] + "  <organism.faa> <organisms.csv> <hmm.hmm> <sqldb.sqlite>")
+		sys.exit(1)  # Aborts program. (exit(1) indicates that an error occurred)
+
+
+# ------------------------------------------------------------------------------------------------------------
 # 2: Runs HMMER with settings specific for extracting subject sequences.
 def runHMMSearch(FASTA, HMMERDBFile):
-	Found16S = True
-	process = subprocess.Popen(["hmmsearch", "--acc", "--cpu", str(processors), HMMERDBFile, "-"], stdin = subprocess.PIPE, stdout = subprocess.PIPE, bufsize = 1)
-	stdout, error = process.communicate(FASTA) # This returns a list with both stderr and stdout. Only want stdout which is first element.
+	process = subprocess.Popen(["hmmsearch", "--acc", "--cpu", str(processors), HMMERDBFile, "-"],
+	                           stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
+	stdout, error = process.communicate(
+		FASTA)  # This returns a list with both stderr and stdout. Only want stdout which is first element.
 	if error:
-		print error
+		print(str(error))
 		sys.exit(1)
 	else:
 		return stdout
-#------------------------------------------------------------------------------------------------------------
-# 3: Creates a python dictionary (hash table) that contains the the fasta for each protien in the proteome.
+
+
+# ------------------------------------------------------------------------------------------------------------
+# 3: Creates a python dictionary (hash table) that contains the the fasta for each protein in the proteome.
 def createProteomeHash(ProteomeFile):
-	ProteomeHash = dict() 
+	ProteomeHash = dict()
 	try:
 		handle = open(ProteomeFile, "rU")
 		proteome = SeqIO.parse(handle, "fasta")
 		for record in proteome:
-			ProteomeHash.update({ record.id : record.format("fasta") })
+			ProteomeHash.update({record.id: record.format("fasta")})
 		handle.close()
 	except IOError:
-		print "Failed to open " + ProteomeFile
+		print("Failed to open " + ProteomeFile)
 		exit(1)
 	return ProteomeHash
-#------------------------------------------------------------------------------------------------------------
-# 4: Parses HMM searches text output and generates a two dementional array of the domain alignments results.
+
+
+# ------------------------------------------------------------------------------------------------------------
+# 4: Parses HMM searches text output and generates a two dimensional array of the domain alignments results.
 def parseHmmsearchResults(HMMResults, HMMName, HMMLength):
 	HitRowRegex = re.compile("^\s*\d\s*((\?)|(\!))\s*")
-	HMMResults = HMMResults.split(">>") # Splits output at domain alignments.
-	del HMMResults[0] # Deletes stuff at top of text output which would be the first element after the split.
-	HMMResults = [x.split("Alignments")[0] for x in HMMResults] # Removes detailed per alignment info.
+	HMMResults = HMMResults.split(">>")  # Splits output at domain alignments.
+	del HMMResults[0]  # Deletes stuff at top of text output which would be the first element after the split.
+	HMMResults = [x.split("Alignments")[0] for x in HMMResults]  # Removes detailed per alignment info.
 	HMMResultsCleaned = []
 	for proteinResult in HMMResults:
 		proteinResult = proteinResult.splitlines()
-		TargetProtein = proteinResult[0].split()[0] # Records protein accession from first line
+		TargetProtein = proteinResult[0].split()[0]  # Records protein accession from first line
 		for row in proteinResult:
-			if HitRowRegex.match(row): # If row is a domain table line.
+			if HitRowRegex.match(row):  # If row is a domain table line.
 				row = row.split()
-				score   = float(row[2])
-				evalue  = float(row[5])
+				score = float(row[2])
+				evalue = float(row[5])
 				hmmfrom = int(row[6])
-				hmmto   = int(row[7])
+				hmmto = int(row[7])
 				alifrom = int(row[9])
-				alito   = int(row[10])
-				hmmCoverage = ((float((hmmto - hmmfrom)))/float((HMMLength)))
+				alito = int(row[10])
+				hmmCoverage = ((float((hmmto - hmmfrom))) / float((HMMLength)))
 				DomainRow = [TargetProtein, HMMName, score, evalue, hmmfrom, hmmto, alifrom, alito, hmmCoverage]
 				HMMResultsCleaned.append(DomainRow)
 	return HMMResultsCleaned
-#------------------------------------------------------------------------------------------------------------
-# 6: Fitres HMM Hits.
-def filterHMMHitTable(HMMHitTable):
 
-	HMMHitTable = [row for row in HMMHitTable if row[3] < float("1e-25")] # Filtres by E-value.
-	
+
+# ------------------------------------------------------------------------------------------------------------
+# 6:  HMM Hits.
+def filterHMMHitTable(HMMHitTable):
+	HMMHitTable = [row for row in HMMHitTable if row[3] < float("1e-25")]  # Filters by E-value.
+
 	i = 0
-	while i < (len(HMMHitTable)-1):
-		RowOne = HMMHitTable[i]    # Current Row in hit table.
-		RowTwo = HMMHitTable[i+1]  # Row below.
-		if(RowOne[0] == RowTwo[0]): # If they have the same targe protein.
-			AlignmentOneLength = RowOne[-2] - RowOne[-3] # RowOne AliTo - AliFrom
-			AlignmentTwoLength = RowTwo[-2] - RowTwo[-3] # RowTwo AliTo - AliFrom
+	while i < (len(HMMHitTable) - 1):
+		RowOne = HMMHitTable[i]  # Current Row in hit table.
+		RowTwo = HMMHitTable[i + 1]  # Row below.
+		if RowOne[0] == RowTwo[0]:  # If they have the same targe protein.
+			AlignmentOneLength = RowOne[-2] - RowOne[-3]  # RowOne AliTo - AliFrom
+			AlignmentTwoLength = RowTwo[-2] - RowTwo[-3]  # RowTwo AliTo - AliFrom
 			Overlap = RowOne[-2] - RowTwo[-3]  # RowOne AliTo -  RowTwo AliFrom
-			if (Overlap > 0): # If there is overlap...
+			if Overlap > 0:  # If there is overlap...
 				# If the overlap is greater than 50% of either alignment.
-				if((((float(Overlap)/float(AlignmentOneLength)) > 0.5) or ((float(Overlap)/float(AlignmentTwoLength)) > 0.5))):
-					if RowOne[3] < RowTwo[3]: # If row one has a lower E-value than row two remove row two else remove row one.
+				if ((float(Overlap) / float(AlignmentOneLength)) > 0.5) or (
+							(float(Overlap) / float(AlignmentTwoLength)) > 0.5):
+					if RowOne[3] < RowTwo[
+						3]:  # If row one has a lower E-value than row two remove row two else remove row one.
 						HMMHitTable.remove(RowTwo)
 					else:
 						HMMHitTable.remove(RowOne)
-					i = i - 1 # Resets list index.
+					i -= 1  # Resets list index.
 		i += 1
-		
-	HMMHitTable = [row for row in HMMHitTable if row[-1] > 0.3] # Filtres by Query Coverage.
-	return	HMMHitTable		
-#------------------------------------------------------------------------------------------------------------
-# 6: Creates list of hits protien FASTAs.
+
+	HMMHitTable = [row for row in HMMHitTable if row[-1] > 0.3]  # Filters by Query Coverage.
+	return HMMHitTable
+
+
+# ------------------------------------------------------------------------------------------------------------
+# 6: Creates list of hits protein FASTAs.
 def getHitProteins(HMMHitTable, AnnotationFASTADict, OrganismName):
 	HitProteins = []
 	for row in HMMHitTable:
 		ProteinAccession = row[0]
 		ProteinFASTA = AnnotationFASTADict[ProteinAccession]
 
-		Locus = LocusRegex.search(ProteinFASTA).group(0)
+		Locus = str(LocusRegex.search(ProteinFASTA).group(0))
 		Locus = Locus.split()[1].rstrip(")")
+		LocationData = LocationRegex.search(ProteinFASTA)
 		try:
-			LocationData = LocationRegex.search(ProteinFASTA)
-			Start  = int(LocationData.group(1))
-			End    = int(LocationData.group(2))
+			Start = int(LocationData.group(1))
+			End = int(LocationData.group(2))
 			Strand = LocationData.group(3)
 			ProteinData = [ProteinAccession, OrganismName, Locus, Start, End, Strand, ProteinFASTA]
 			HitProteins.append(ProteinData)
-		except AttributeError as Error:
-			print row
-			print ProteinFASTA
-			print LocationData
-			print "This is the organism: ", OrganismName
-			print "The AttributeError was ", str(Error)
+		except AttributeError as error:
+			print(row)
+			print(ProteinFASTA)
+			print(LocationData)
+			print("This is the organism: ", OrganismName)
+			print("The AttributeError was ", str(error))
 			sys.exit(1)
 	return HitProteins
-#-----------------------------------------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------------------------------------
 # 7: Inserts organism info into DB.
-def insertOrganismInfo(cursor, OrganismInfo):
-	cursor.execute('''INSERT OR REPLACE INTO Organisms(Organism_Accession, Accession_Type, Organism_Description, Source, Organism_Phylogeny, Sequence_Length)
-			          VALUES(?,?,?,?,?,?)''', OrganismInfo)
-#-----------------------------------------------------------------------------------------------------------
+def insertOrganismInfo(db_cursor, OrganismInfo):
+	query = '''INSERT OR REPLACE INTO Organisms
+(
+	Organism_Accession,
+	Accession_Type,
+	Organism_Description,
+	Source,
+	Organism_Phylogeny,
+	Sequence_Length
+)
+VALUES
+	(?,?,?,?,?,?)'''
+
+	db_cursor.execute(query, OrganismInfo)
+
+
+# -----------------------------------------------------------------------------------------------------------
 # 8: Inserts protein info into DB.
-def insertProteins(cursor, HitProteins):
+def insertProteins(db_cursor, HitProteins):
+	query = '''INSERT OR REPLACE INTO Proteins
+(
+	Protein_Accession,
+	Organism_Accession,
+	Locus,
+	Start,
+	"End",
+	Strand,
+	FASTA_Sequence
+)
+VALUES
+	(?,?,?,?,?,?,?)'''
+
 	for protein in HitProteins:
-		cursor.execute('''INSERT OR REPLACE INTO Proteins(Protein_Accession,Organism_Accession,Locus,Start,End,Strand,FASTA_Sequence)
-		                  VALUES(?,?,?,?,?,?,?)''', protein)
-#-----------------------------------------------------------------------------------------------------------
+		db_cursor.execute(query, protein)
+
+
+# -----------------------------------------------------------------------------------------------------------
 # 9: Inserts hits into DB and creates md5 hash for primary key.
 def insertHits(cursor, HMMHitTable):
+	query = '''INSERT OR REPLACE INTO HMM_Hits
+(
+	Hit_HASH,
+	Protein_Accession,
+	HMM_Model,
+	HMM_Score,
+	HMM_E_Value,
+	Ali_From,
+	Ali_To,
+	HMM_From,
+	HMM_To,
+	HMM_Coverage
+)
+VALUES
+	(?,?,?,?,?,?,?,?,?,?)'''
+
 	HitHash = hashlib.md5()
 	for hit in HMMHitTable:
-		HitHash.update("".join([str(i) for i in HMMHitTable])) # Converts every list element to a string, joins them and updates Hash.
+		HitHash.update("".join(
+			[str(i) for i in HMMHitTable]))  # Converts every list element to a string, joins them and updates Hash.
 		hit = [HitHash.hexdigest()] + hit
-		cursor.execute('''INSERT OR REPLACE INTO HMM_Hits(Hit_HASH, Protein_Accession, HMM_Model, HMM_Score, HMM_E_Value, Ali_From, Ali_To, HMM_From, HMM_To, HMM_Coverage) 
-			              VALUES(?,?,?,?,?,?,?,?,?,?)''', hit)
-#===========================================================================================================
+		cursor.execute(query, hit)
+
+
+# ===========================================================================================================
 # Main program code:
-	
+
 # House keeping...
-argsCheck(5) # Checks if the number of arguments are correct.
+argsCheck(5)  # Checks if the number of arguments are correct.
 
 # Stores file one for input checking.
-print ">> Starting up..."
+print(">> Starting up...")
 OrganismFile = sys.argv[1]
 PhyloCSVFile = sys.argv[2]
-HMMFile      = sys.argv[3]
-sqlFile      = sys.argv[4]
+HMMFile = sys.argv[3]
+sqlFile = sys.argv[4]
 
 HMMName = path.split(HMMFile)[1].rstrip(".hmm")
 OrganismName = path.split(OrganismFile)[1].rstrip(".faa")
 
 # File extension checks
-print ">> Performing file extention checks..."
+print(">> Performing file extension checks...")
 if not OrganismFile.endswith(".faa"):
-	print "[Warning] " + OrganismFile + " may not be a fasta file!"
+	print("[Warning] " + OrganismFile + " may not be a fasta file!")
 if not PhyloCSVFile.endswith(".csv"):
-	print "[Warning] " + PhyloCSVFile + " may not be a csv file!"
+	print("[Warning] " + PhyloCSVFile + " may not be a csv file!")
 if not HMMFile.endswith(".hmm"):
-	print "[Warning] " + HMMFile + " may not be a HMM file!"
+	print("[Warning] " + HMMFile + " may not be a HMM file!")
 if not sqlFile.endswith(".sqlite"):
-	print "[Warning] " + sqlFile + " may not be a sqlite file!"
+	print("[Warning] " + sqlFile + " may not be a sqlite file!")
 
 # Read in genbank file as a sequence record object.
 try:
-	print ">> Opening FASTA File: " + OrganismFile
+	print(">> Opening FASTA File: " + OrganismFile)
 	handle = open(OrganismFile, "rU")
 	records = SeqIO.parse(handle, "fasta")
 	handle.close()
 except IOError:
-	print "Failed to open " + OrganismFile
+	print("Failed to open " + OrganismFile)
 	sys.exit(1)
-	
+
 # Opens OrganismDB CSV file for reading and stores as hash table.
 OrganismHash = {}
 try:
-	print ">> Opening CSV File: " + PhyloCSVFile
+	print(">> Opening CSV File: " + PhyloCSVFile)
 	readFile = open(PhyloCSVFile, "r")
-	reader  = csv.reader(readFile) # Opens file with csv module which takes into account verying csv formats and parses correctly.
-	print ">> Good CSV file."
+	reader = csv.reader(
+		readFile)  # Opens file with csv module which takes into account verying csv formats and parses correctly.
+	print(">> Good CSV file.")
 	for row in reader:
 		OrganismHash[row[0]] = row
 	readFile.close()
 except IOError:
-	print "Failed to open " + PhyloCSVFile
+	print("Failed to open " + PhyloCSVFile)
 	sys.exit(1)
 
 # Gets HMM Length.
 try:
 	HMMLengthRegex = re.compile("^LENG\s*\d*$")
-	print ">> Opening HMM File: " + HMMFile
+	print(">> Opening HMM File: " + HMMFile)
 	with open(HMMFile, "rU") as inFile:
 		currentLine = inFile.readline()
 		while not HMMLengthRegex.match(currentLine):
 			currentLine = inFile.readline()
 		HMMLength = int(currentLine.split()[1])
 except IOError:
-	print "Failed to open " + HMMFile
+	print("Failed to open " + HMMFile)
 	sys.exit(1)
-	
-print ">> Extracting Protein Annotations..."
-AnnotationFASTADict = createProteomeHash(OrganismFile) # Creates a dictionary containing all protein annotations in the gbk file.
 
-print ">> Extracting Organism Info..."
+print(">> Extracting Protein Annotations...")
+AnnotationFASTADict = createProteomeHash(
+	OrganismFile)  # Creates a dictionary containing all protein annotations in the gbk file.
+
+print(">> Extracting Organism Info...")
 OrganismInfo = OrganismHash[OrganismName]
 
-print ">> Running Hmmsearch..."
-FASTAString = "".join(AnnotationFASTADict.values()) # Saves these annotations to a string.
-HMMResults  = runHMMSearch(FASTAString, HMMFile) # Runs hmmsearch.
+print(">> Running Hmmsearch...")
+FASTAString = "".join(AnnotationFASTADict.values())  # Saves these annotations to a string.
+HMMResults = runHMMSearch(FASTAString, HMMFile)  # Runs hmmsearch.
 
-if (len(HMMResults) < 1):
-	print "Hmmearch found no hits for " + HMMFile + " in " + OrganismName
-	print "Aborting..."
+if len(HMMResults) < 1:
+	print("Hmmearch found no hits for " + HMMFile + " in " + OrganismName)
+	print("Aborting...")
 	sys.exit(1)
 
-print ">> Parsing and filtering hmmsearch results..."
-HMMHitTable = parseHmmsearchResults(HMMResults, HMMName, HMMLength) # Parses hmmsearch results into a two dimensional array.
+print(">> Parsing and filtering hmmsearch results...")
+HMMHitTable = parseHmmsearchResults(HMMResults, HMMName,
+                                    HMMLength)  # Parses hmmsearch results into a two dimensional array.
 HMMHitTable = filterHMMHitTable(HMMHitTable)
 
-if (len(HMMHitTable) < 1):
-	print "We found no quality hits for " + HMMFile + " in " + OrganismName
-	print "Aborting..."
+if len(HMMHitTable) < 1:
+	print("We found no quality hits for " + HMMFile + " in " + OrganismName)
+	print("Aborting...")
 	sys.exit(1)
 
-print ">> Extracting Hit Proteins."
-HitProteins = getHitProteins(HMMHitTable, AnnotationFASTADict, OrganismName) # Gets hit protein FASTAs.
+print(">> Extracting Hit Proteins.")
+HitProteins = getHitProteins(HMMHitTable, AnnotationFASTADict, OrganismName)  # Gets hit protein FASTAs.
 
 if path.isfile(sqlFile):
 	try:
-		HMMDB = sqlite3.connect(sqlFile, timeout = 3600)
-		print ">> Opened database successfully."
+		HMMDB = sqlite3.connect(sqlFile, timeout=3600)
+		print(">> Opened database successfully.")
 		cursor = HMMDB.cursor()
-		
-		print ">> Inserting Organism Info."
+
+		print(">> Inserting Organism Info.")
 		insertOrganismInfo(cursor, OrganismInfo)
-		print ">> Inserting Proteins."
+		print(">> Inserting Proteins.")
 		insertProteins(cursor, HitProteins)
-		print ">> Inserting Hits."
+		print(">> Inserting Hits.")
 		insertHits(cursor, HMMHitTable)
-			
+
 		HMMDB.commit()
 		HMMDB.close()
 	except sqlite3.Error as Error:
-		print "sqlite3 Error: " + str(Error)
-		print "The program will be aborted."
-		sys.exit(1)		
+		print("sqlite3 Error: " + str(Error))
+		print("The program will be aborted.")
+		sys.exit(1)
 else:
-	print "Failed to open " + sqlFile
+	print("Failed to open " + sqlFile)
 	sys.exit(1)
-print ">> Done!" 
+print(">> Done!")
